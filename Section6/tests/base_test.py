a4d5ab5d6b637d13c3ec3bcee72779018d545458
Carlos Martinez <alberto-cmg@outlook.es>
2023-05-27 11:09:17 -0600

    section 6 done

--- Section6/tests/base_test.py
+++ Section6/tests/base_test.py
@@ -1,8 +1,8 @@
 """
 BaseTest
 
-This class should be the parent class to each non-unit test.
-It allows for instantiation of the database dynamically
+This class should be the parent class to each unit test.
+It allows for instantiation of the database dynamically,
 and makes sure that it is a new, blank database each time.
 """
 
@@ -12,19 +12,17 @@ from db import db
 
 
 class BaseTest(TestCase):
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
     def setUp(self):
-        # Make sure database exists
-        # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/test'
+        app.config['SQLALCHEMY_DATABASE_URI'] = BaseTest.SQLALCHEMY_DATABASE_URI
         with app.app_context():
             db.init_app(app)
             db.create_all()
-        # Get a test client
         self.app = app.test_client()
         self.app_context = app.app_context
 
     def tearDown(self):
-        # Database is blank
         with app.app_context():
             db.session.remove()
             db.drop_all()
